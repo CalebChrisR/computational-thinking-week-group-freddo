1febcc6ee1324d8acc0fe39f5d50932c35422da5
CalebChrisR <christopher.rojer@gmail.com>
2026-08-31 12:11:53 +0200

    combined all names to say hi

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,2 +1,12 @@
 import filec, christina2, file_amalia, fileh, green, iris
 
+def func1():
+    print ("This is team Freddo, we are:")
+    filec.name()
+    christina2.name()
+    file_amalia.name()
+    fileh.name()
+    green.name()
+    iris.name()
+
+func1()
