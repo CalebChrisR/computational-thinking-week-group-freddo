8fad63838f2abf12ceaa177bb74f7133c9e07576
CalebChrisR <christopher.rojer@gmail.com>
2026-09-01 15:03:44 +0200

    error with notebook

--- challenge_day2/station2.py
+++ challenge_day2/station2.py
@@ -1,6 +1,5 @@
-from datetime import datetime
-
 def solution_station_2(date_string, date_format="%Y-%m-%d"):
+    from datetime import datetime
     japanese_days = ["月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日", "日曜日"]
     dates = datetime.strptime(date_string, date_format)
     weekday_index = dates.weekday()
