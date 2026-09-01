744e50ca24da6e956782e41f199ff82a4372ad86
CalebChrisR <christopher.rojer@gmail.com>
2026-09-01 15:25:46 +0200

    fixed 6

--- challenge_day2/station6.py
+++ challenge_day2/station6.py
@@ -1,7 +1,10 @@
 import math
 
-def solution_station_6(input_variable):
+def solution_station_6(input_variable: float):
     result = math.sin(input_variable)
-    print(round(result,4))
+    return round(result,4)
+
+if __name__ == "__main__":
+    print(solution_station_6(1.7))
 
 
