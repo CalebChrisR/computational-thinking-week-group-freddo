7e5548a3b93b17bb7a5dee7db26165d84bdca7a7
unit-fam <helenkttw@gmail.com>
2026-09-01 14:47:58 +0200

    addign station 5 lists

---
+++ challenge_day2/station5.py
@@ -0,0 +1,4 @@
+def solution_station_5(name):
+    lt1 = ["Ainas", "Ben", "Christopher", "Ebony", "Iuliia", "Klementyna", "Tiara", "Tobit", "Yasmin", "Yurui", "Yuvraj", "Zoë", "Lula", "Markus", "Mateo", "Mufang", "Muni", "Nandini", "Nathan", "Oumaima"]
+    lt2 = ["Alex", "Arwen", "Christina", "David", "Helen", "Huy Bao", "Iris", "Katharina", "Lora", "Mark", "Mats", "Minseo", "Quinn", "Rajko", "Sade", "Sylwia", "Tarling", "Vadim", "Zeno"]
+    lt4 = ["An", "Yujie", "Douwe", "Jeremy", "Krishiv", "Lara", "Heer", "Illya", "Lucas", "Maria", "Michelle", "Neel", "Oliwia", "Paige", "Rakin", "Rapolas", "Samir", "Tom", "Yutong", "Amalia"]
