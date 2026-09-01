0bc8c751e4e99e1a448b69ddd7a828b4c4770d66
“cvana214” <chrisiva3@gmail.com>
2026-09-01 14:34:55 +0200

    changed some station 3

--- challenge_day2/station3.py
+++ challenge_day2/station3.py
@@ -1,2 +1,10 @@
-print("this is station 3")
+print("this is the solution to station 3")
+# if number is dividable by 3, it is true, if not it is false.
+
+print("Choose a number:")
+
+def is_divisible_by_3(number):
+    # Checks if the integer is divisible by 3
+    # Returns True if divisible, else False
+    return number % 3 == 0
 
