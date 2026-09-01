cbfd2731a16a745a36144185e9a8e670141c9810
irisklok <iris.studie@tetka.net>
2026-09-01 14:46:52 +0200

    function station7 completed

---
+++ challenge_day2/station7.py
@@ -0,0 +1,29 @@
+def solution_station_7(sequence):
+    split_apart = list(sequence)
+    for i in range(len(split_apart)):
+        if split_apart[i] == "a":
+            split_apart[i] = "3"
+        if split_apart[i] == "b":
+            split_apart[i] = "-1"
+        if split_apart[i] == "c":
+            split_apart[i] = "4"
+        if split_apart[i] == "d":
+            split_apart[i] = "7"
+        if split_apart[i] == "e":
+            split_apart[i] = "0.5"
+    string_final = "".join(split_apart)        
+    answer = eval(string_final)
+    return answer
+
+'''
+a = 3
+b = -1
+c = 4
+d = 7
+e = 0.5
+
+make all characters separate in the string
+if abcde replace with number
+make math symbols work as math
+do math
+''' 
