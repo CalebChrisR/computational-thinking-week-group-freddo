2e7cbc48d362c596c4ad0e297ab502f4e5069cfe
“natalykranou-beep” <natalykranou@gmail.com>
2026-09-01 14:30:57 +0200

    debug

--- challenge_day2/station2.py
+++ challenge_day2/station2.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 def day_week(date_string, date_format="%Y-%m-%d"):
-    Japanese_days = ["月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日", "日曜日"]
+    japanese_days = ["月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日", "日曜日"]
     dates = datetime.strptime(date_string, date_format)
     weekday_index = dates.weekday()
     
