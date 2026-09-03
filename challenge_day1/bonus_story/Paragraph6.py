4e2ad79511e5a05cc7536c8bc9540168c0df6174
unit-fam <helenkttw@gmail.com>
2026-09-04 00:31:57 +0200

    added steve paragraphs

---
+++ challenge_day1/bonus_story/Paragraph6.py
@@ -0,0 +1,22 @@
+def act1_par6():
+    print('Steve cannot stand the screaming, “I am out of here,” so he twists '
+    'the witch hat, and disappears. c1 is so startled that he stops screaming. '
+    'c4 gets a little scared, and cradles c1 closer. c3, inspired by c1, starts '
+    'singing “born this way.” The c2 is not confused, and just chills. c5 is a bit ' 
+    'upset they are no longer singing c5.')
+
+def act2_par6():
+    print('Once Steve has finished, he joins c4 and the c2 at the window, watching '
+    'as c1 is pulled away to die. c5 looks a little pained, but not that repentant. ' 
+    'They ask Steve, “how did you know?” c3 listens in. Steve mumbles, “what a wicked ' 
+    'problem,” and twists the hat to disappear.')
+
+def act3_par6():
+    print('Steve, upon seeing that they are all happy, pops his guitar away and bows. '
+    '“I will see you next time.” With a twist of his hat, he disappears once more, '
+    'travelling through time again. The c2 and c3 continue their concert. c4, c5, '
+    'and c1 remain together, happy that they are together again. ')
+
+act1_par6()
+act2_par6()
+act3_par6()
