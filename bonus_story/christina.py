fbe7119877512d954dc6624d1418ed95509c8305
“cvana214” <chrisiva3@gmail.com>
2026-09-03 18:41:04 +0200

    took out names

--- bonus_story/christina.py
+++ bonus_story/christina.py
@@ -1,20 +1,20 @@
 
 # Act 1
 def act1_par3 ():
-    print("As Mary predicted, jeSUS starts screaming, because he is a newborn baby. " \
-"The Holy Spirit given him strength to scream loud. Lady Gaga cannot outsing Jesus right now, " \
-"and Judas is perplexed by how loud Jesus is. Steve pulls down the witch hat over his ears to block out the noise.") 
+    print("As c4 predicted, c1 starts screaming, because he is a newborn baby. " \
+"The c2 gives him strength to scream loud. Lady Gaga cannot outsing c1 right now, " \
+"and c5 is perplexed by how loud c1 is. c6 pulls down the witch hat over his ears to block out the noise.") 
 
 # Act 2
 def act2_par3 ():
-    print("Steve nods, “I am sure it is…” He indicates to the food, “I want your everything as long as it is free.” Jesus gestures" \
-    " at the food and nods. Realising that another person is joining their dinner, the Holy Spirit sets a new place for Steve." \
-    " Judas starts getting nervous as he realises that time is running out for his betrayal. Mary and Lady Gaga are still gossiping about the local dog race.")
+    print("c6 nods, “I am sure it is…” He indicates to the food, “I want your everything as long as it is free.” c1 gestures" \
+    " at the food and nods. Realising that another person is joining their dinner, the c2 sets a new place for c6." \
+    " c5 starts getting nervous as he realises that time is running out for his betrayal. c4 and Lady Gaga are still gossiping about the local dog race.")
 
 
 
 # Act 3
 def act3_par3 ():
-    print("The Holy Spirit calls on all the good memories they shared with Jesus and the gang." \
-    " They think of Mary’s devotion, Lady Gaga’s singing prowess, Judas and Jesus’ relationship, and " \
-    "finally Steve’s eccentricness. With a pop, the wicked witch hat suddenly looks much better. It can probably travel through time now. ")
+    print("The c2 calls on all the good memories they shared with c1 and the gang." \
+    " They think of c4's devotion, Lady Gaga’s singing prowess, c5 and c1' relationship, and " \
+    "finally c6's eccentricness. With a pop, the wicked witch hat suddenly looks much better. It can probably travel through time now. ")
