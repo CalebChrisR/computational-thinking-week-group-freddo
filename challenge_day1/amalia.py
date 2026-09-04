2fb79f84da76ebca33cad37b52d48679b0c9854d
amalia1131 <amalia.gerg@gmail.com>
2026-09-04 10:54:13 +0200

    my func is up too

---
+++ challenge_day1/amalia.py
@@ -0,0 +1,16 @@
+def act1_p5():
+    paragraph = "'I can’t help it c4, I was born this way,' c1 responds and continues crying. c3 gives a proud look, while c6, Judas, and the c2 start clapping."
+    return paragraph
+
+
+def act2_p5():
+    paragraph = "c1 and Judas can still faintly hear c3’s song of Judas, but they try to ignore her. c4 and the c2 watch and Judas kisses c1’ cheek and murmurs something, but are then horrified when c1 is arrested. c6 perks up at the commotion and stops eating."
+    return paragraph
+
+
+def act3_p5(): 
+    paragraph = "There’s a happy shout as c4 and Judas give c1 a hug. c3 bursts into song, accompanying c6’s guitar. 'I can’t remember, but it’s alright a-alright. Just Dance!' The c2 summons a dance floor and lights."
+    return paragraph
+
+
+# then print these functions in order ig
