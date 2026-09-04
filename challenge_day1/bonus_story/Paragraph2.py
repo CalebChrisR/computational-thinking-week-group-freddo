45267217304025a83082021ac85304ce07945ac0
unit-fam <helenkttw@gmail.com>
2026-09-04 12:31:02 +0200

    added steve to p2 + lowercase p

--- challenge_day1/bonus_story/Paragraph2.py
+++ challenge_day1/bonus_story/Paragraph2.py
@@ -3,11 +3,11 @@ def act1_par2 ():
     print("c1 takes a breath. The Holy Spirit flows within c1, and also c4. " \
 "c4 is kind of relieved to be done with the birth. c5 can predict the baby " \
 "screams. c3 knows she's gonna have to sing louder once c1 starts crying. " \
-"c6 stops dancing to get the witch hat on.")
+"Steve stops dancing to get the witch hat on.")
 
 # Act 2
 def act2_par2 ():
-    print("“What?” Asks c1, confused why c6 would call it a last supper. " \
+    print("“What?” Asks c1, confused why Steve would call it a last supper. " \
 "“This is just normal supper.” c5, feeling that his secret is up, buries his face" \
 " in the wine, determined not to think about it. c4 and c3 are a little too in" \
 " their cups, and gossip about who is gonna win the local dog race. The Holy Spirit" \
@@ -16,7 +16,7 @@ def act2_par2 ():
 # Act 3
 def act3_par2 ():
     print("The Holy Spirit realises that they have the power to change this." \
-" “I can fix your hat,” they tell c6. c6 hands over the Witch's hat, drawing the" \
+" “I can fix your hat,” they tell Steve. Steve hands over the Witch's hat, drawing the" \
 " attention of c4, c5, and c3. This is the first time in a week that their" \
 " attention is broken from c1' resting place.")
     
