e8b70341bc632f8bf1cc98dde7eba55aacc24194
“natalykranou-beep” <natalykranou@gmail.com>
2026-09-04 11:15:04 +0200

    nataly fixed spelling

--- challenge_day1/bonus_story/Paragraph2.py
+++ challenge_day1/bonus_story/Paragraph2.py
@@ -2,7 +2,7 @@
 def act1_par2 ():
     print("c1 takes a breath. The Holy Spirit flows within c1, and also c4. " \
 "c4 is kind of relieved to be done with the birth. c5 can predict the baby " \
-"screams. c3 knows she's gonna have to sang louder once c1 starts crying. " \
+"screams. c3 knows she's gonna have to sing louder once c1 starts crying. " \
 "c6 stops dancing to get the witch hat on.")
 
 # Act 2
@@ -18,7 +18,7 @@ def act3_par2 ():
     print("The Holy Spirit realises that they have the power to change this." \
 " “I can fix your hat,” they tell c6. c6 hands over the Witch's hat, drawing the" \
 " attention of c4, c5, and c3. This is the first time in a week that their" \
-" attention is broken from c1' rosting place.")
+" attention is broken from c1' resting place.")
     
 act1_par2()
 act2_par2()
