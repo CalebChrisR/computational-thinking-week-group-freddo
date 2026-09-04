3208580e6feab53db3c969e421b1becb81085274
CalebChrisR <christopher.rojer@gmail.com>
2026-09-04 15:26:25 +0200

    print to return

--- challenge_day1/bonus_story/Paragraph4.py
+++ challenge_day1/bonus_story/Paragraph4.py
@@ -1,13 +1,13 @@
 #ACT 1
 def act1_par4():
-    print ("ch4 asks ch1, “why cant you stop screaming.” " \
+    return ("ch4 asks ch1, “why cant you stop screaming.” " \
     "ch3 stops singing ch5 so that they can hear ch1 answer. " \
     "ch2 passes through ch1 to quiet ch1's screaming so that he can answer. " \
     "ch6 fiddles with his witch hat.")
 
 #ACT 2
 def act2_par4():
-    print ("“Hey, can we talk?” ch5 says and pulls ch1 aside. " \
+    return ("“Hey, can we talk?” ch5 says and pulls ch1 aside. " \
     "They walk to the garden to escape ch3's singing of the song ch5. " \
     "Everyone notices them leaving, and peek out the window. " \
     "ch6, however, knowing what happens, stays sat at the table, enjoying the meal. " \
@@ -16,7 +16,7 @@ def act2_par4():
 
 #ACT 3
 def act3_par4():
-    print ("ch6 takes the hat, and with a pop suAmmons a guitar. " \
+    return ("ch6 takes the hat, and with a pop suAmmons a guitar. " \
     "He starts strumming it, much to the enjoyment of ch2. " \
     "ch3 loves the song, and starts thinking of a way to accompany him. " \
     "ch5 and ch4 turn back to the resting place in awe as the stone at ch1' cave moves until suddenly, ch1 it out. " \
