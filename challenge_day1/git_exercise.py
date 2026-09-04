90c55a5b3fdc3fdd9f4872e680f9f4b84ee5cacb
CalebChrisR <christopher.rojer@gmail.com>
2026-09-04 15:11:25 +0200

    first step at combining in exercsie

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,6 +1,6 @@
 from day_1 import filec, christina2, file_amalia, fileh, green, iris
 
-from bonus_story import Paragraph1, Paragraph2, Paragraph3, Paragraph4, Paragraph5, Paragraph6
+from bonus_story import Paragraph1 as par1, Paragraph2 as par2, Paragraph3 as par3, Paragraph4 as par4, Paragraph5 as par5, Paragraph6 as par6
 
 def func1():
     print ("This is team Freddo, we are:")
@@ -19,29 +19,32 @@ if __name__  == "__main__":
 
     # bonus challenge start
     # act 1
-    Paragraph1.act1_par1()
-    Paragraph2.act1_par2()
-    Paragraph3.act1_par3()
-    Paragraph4.act1_par4()
-    Paragraph5.act1_par5()
-    Paragraph6.act1_par6()
-    print()
+    act1 = ""
+    act1_1 = par1.act1_par1()
+    act1_2 = par2.act1_par2()
+    act1_3 = par3.act1_par3()
+    act1_4 = par4.act1_par4()
+    act1_5 = par5.act1_par5()
+    act1_6 = par6.act1_par6()
+
+    act1 = act1_1 + act1_2 + act1_3 + act1_4 + act1_5 + act1_6
+    print(act1)
 
     # act 2
-    Paragraph1.act2_par1()
-    Paragraph2.act2_par2()
-    Paragraph3.act2_par3()
-    Paragraph4.act2_par4()
-    Paragraph5.act2_par5()
-    Paragraph6.act2_par6()    
+    par1.act2_par1()
+    par2.act2_par2()
+    par3.act2_par3()
+    par4.act2_par4()
+    par5.act2_par5()
+    par6.act2_par6()    
     print()
 
     # act 3
-    Paragraph1.act3_par1()
-    Paragraph2.act3_par2()
-    Paragraph3.act3_par3()
-    Paragraph4.act3_par4()
-    Paragraph5.act3_par5()
-    Paragraph6.act3_par6()    
+    par1.act3_par1()
+    par2.act3_par2()
+    par3.act3_par3()
+    par4.act3_par4()
+    par5.act3_par5()
+    par6.act3_par6()    
     print()
 
