0ecd210c275049be22739ff06f842f7a7ccd6fa2
CalebChrisR <christopher.rojer@gmail.com>
2026-09-04 15:12:25 +0200

    change prints to return

--- challenge_day1/bonus_story/Paragraph4.py
+++ challenge_day1/bonus_story/Paragraph4.py
@@ -1,13 +1,13 @@
 #ACT 1
 def act1_par4():
-    print ("Mary asks Jesus, 'why cant you stop screaming.' " \
+    return ("Mary asks Jesus, 'why cant you stop screaming.' " \
     "Lady Gaga stops singing Judas so that they can hear Jesus answer. " \
     "Holy Spirit passes through Jesus to quiet Jesus’ screaming so that he can answer. " \
     "Steve fiddles with his witch hat.")
 
 #ACT 2
 def act2_par4():
-    print ("'Hey, can we talk?' Judas says and pulls Jesus aside. " \
+    return ("'Hey, can we talk?' Judas says and pulls Jesus aside. " \
     "They walk to the garden to escape Lady Gaga's singing of the song Judas. " \
     "Everyone notices them leaving, and peek out the window. " \
     "Steve, however, knowing what happens, stays sat at the table, enjoying the meal. " \
@@ -16,7 +16,7 @@ def act2_par4():
 
 #ACT 3
 def act3_par4():
-    print ("Steve takes the hat, and with a pop summons a guitar. " \
+    return ("Steve takes the hat, and with a pop summons a guitar. " \
     "He starts strumming it, much to the enjoyment of Holy Spirit. " \
     "Lady Gaga loves the song, and starts thinking of a way to accompany him. " \
     "Judas and Mary turn back to the resting place in awe as the stone at Jesus’ cave moves until suddenly, Jesus it out. " \
--- challenge_day1/bonus_story/Paragraph5.py
+++ challenge_day1/bonus_story/Paragraph5.py
@@ -1,19 +1,19 @@
 #ACT 1
 def act1_par5():
-    print ("'I can’t help it Mary, I was born this way,' Jesus responds and continues crying. " \
+    return ("'I can’t help it Mary, I was born this way,' Jesus responds and continues crying. " \
     "Lady Gaga gives a proud look, while Steve, Judas, and the Holy Spirit start clapping.")
 
 
 #ACT 2
 def act2_par5():
-    print ("Jesus and Judas can still faintly hear Lady Gaga’s song of Judas, but they try to ignore her. " \
+    return ("Jesus and Judas can still faintly hear Lady Gaga’s song of Judas, but they try to ignore her. " \
     "Mary and the Holy Spirit watch and Judas kisses Jesus’ cheek and murmurs something, " \
     "but are then horrified when Jesus is arrested. Steve perks up at the commotion and stops eating.")
 
 
 #ACT 3
 def act3_par5(): 
-    print ("There’s a happy shout as Mary and Judas give Jesus a hug. " \
+    return ("There’s a happy shout as Mary and Judas give Jesus a hug. " \
     "Lady Gaga bursts into song, accompanying Steve’s guitar. " \
     "'I can’t remember, but it’s alright a-alright. Just Dance!' " \
     "The Holy Spirit summons a dance floor and lights.")
