0b086682f267e139f7c6cfade1336a34b4ed07aa
CalebChrisR <christopher.rojer@gmail.com>
2026-09-04 15:19:50 +0200

    judas function replace

--- challenge_day1/bonus_story/Paragraph5.py
+++ challenge_day1/bonus_story/Paragraph5.py
@@ -1,25 +1,31 @@
 #ACT 1
 def act1_par5():
     return ("'I can’t help it ch4, I was born this way,' ch1 responds and continues crying. " \
-    "ch3 gives a proud look, while ch6, Judas, and the ch2 start clapping.")
+    "ch3 gives a proud look, while ch6, ch5, and the ch2 start clapping.")
 
 
 #ACT 2
 def act2_par5():
-    return ("ch1 and Judas can still faintly hear ch3’s song of Judas, but they try to ignore her. " \
-    "ch4 and the ch2 watch and Judas kisses ch1’ cheek and murmurs something, " \
+    return ("ch1 and ch5 can still faintly hear ch3’s song of ch5, but they try to ignore her. " \
+    "ch4 and the ch2 watch and ch5 kisses ch1’ cheek and murmurs something, " \
     "but are then horrified when ch1 is arrested. ch6 perks up at the commotion and stops eating.")
 
 
 #ACT 3
 def act3_par5(): 
-    return ("There’s a happy shout as ch4 and Judas give Jesus a hug. " \
+    return ("There’s a happy shout as ch4 and ch5 give ch1 a hug. " \
     "ch3 bursts into song, accompanying ch6's guitar. " \
     "'I can’t remember, but it’s alright a-alright. Just Dance!' " \
     "The ch2 summons a dance floor and lights.")
+
+
+def replace_Judas(text: str):
+    text.replace("ch5", "Judas")
+    return text
 
 if __name__ == "__main__":
     act1_par5()
     act2_par5()
     act3_par5()
     pass
+
