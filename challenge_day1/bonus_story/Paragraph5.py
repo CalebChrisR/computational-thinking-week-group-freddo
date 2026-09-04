0af946f2046bd5bb51b02041337deb5637b3f118
amalia1131 <amalia.gerg@gmail.com>
2026-09-04 11:04:43 +0200

    my file is there

--- challenge_day1/bonus_story/Paragraph5.py
+++ challenge_day1/bonus_story/Paragraph5.py
@@ -1,16 +1,24 @@
-def act1_p5():
-    paragraph = "'I can’t help it c4, I was born this way,' c1 responds and continues crying. c3 gives a proud look, while c6, Judas, and the c2 start clapping."
-    return paragraph
+#ACT 1
+def act1_par5():
+    print( "'I can’t help it c4, I was born this way,' c1 responds and continues crying. "
+    "c3 gives a proud look, while c6, Judas, and the c2 start clapping.")
 
 
-def act2_p5():
-    paragraph = "c1 and Judas can still faintly hear c3’s song of Judas, but they try to ignore her. c4 and the c2 watch and Judas kisses c1’ cheek and murmurs something, but are then horrified when c1 is arrested. c6 perks up at the commotion and stops eating."
-    return paragraph
+#ACT 2
+def act2_par5():
+    print("c1 and Judas can still faintly hear c3’s song of Judas, but they try to ignore her. " \
+    "c4 and the c2 watch and Judas kisses c1’ cheek and murmurs something, " \
+    "but are then horrified when c1 is arrested. c6 perks up at the commotion and stops eating.")
 
 
-def act3_p5(): 
-    paragraph = "There’s a happy shout as c4 and Judas give c1 a hug. c3 bursts into song, accompanying c6’s guitar. 'I can’t remember, but it’s alright a-alright. Just Dance!' The c2 summons a dance floor and lights."
-    return paragraph
+#ACT 3
+def act3_par5(): 
+    print("There’s a happy shout as c4 and Judas give c1 a hug. " \
+    "c3 bursts into song, accompanying c6’s guitar. " \
+    "'I can’t remember, but it’s alright a-alright. Just Dance!' " \
+    "The c2 summons a dance floor and lights.")
 
 
-# then print these functions in order ig
+act1_par5()
+act2_par5()
+act3_par5()
