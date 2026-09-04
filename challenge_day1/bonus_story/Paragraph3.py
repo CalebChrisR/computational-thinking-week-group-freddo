9804baf4515f17022987b27487bb938e1ec821d4
unit-fam <helenkttw@gmail.com>
2026-09-04 12:44:56 +0200

    added steve to p3+lowercase

--- challenge_day1/bonus_story/Paragraph3.py
+++ challenge_day1/bonus_story/Paragraph3.py
@@ -3,12 +3,12 @@
 def act1_par3 ():
     print("As c4 predicted, c1 starts screaming, because he is a newborn baby. " \
 "The c2 gives him strength to scream loud. Lady Gaga cannot outsing c1 right now, " \
-"and Judas is perplexed by how loud c1 is. c6 pulls down the witch hat over his ears to block out the noise.") 
+"and Judas is perplexed by how loud c1 is. Steve pulls down the witch hat over his ears to block out the noise.") 
 
 # Act 2
 def act2_par3 ():
-    print("c6 nods, “I am sure it is…” He indicates to the food, “I want your everything as long as it is free.” c1 gestures" \
-    " at the food and nods. Realising that another person is joining their dinner, the c2 sets a new place for c6." \
+    print("Steve nods, “I am sure it is…” He indicates to the food, “I want your everything as long as it is free.” c1 gestures" \
+    " at the food and nods. Realising that another person is joining their dinner, the c2 sets a new place for Steve." \
     " Judas starts getting nervous as he realises that time is running out for his betrayal. c4 and Lady Gaga are still gossiping about the local dog race.")
 
 
@@ -17,7 +17,7 @@ def act2_par3 ():
 def act3_par3 ():
     print("The c2 calls on all the good memories they shared with c1 and the gang." \
     " They think of c4's devotion, Lady Gaga’s singing prowess, Judas and c1' relationship, and " \
-    "finally c6's eccentricness. With a pop, the wicked witch hat suddenly looks much better. It can probably travel through time now. ")
+    "finally Steve's eccentricness. With a pop, the wicked witch hat suddenly looks much better. It can probably travel through time now. ")
 
 act1_par3()
 act2_par3()
