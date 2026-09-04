9e0a57da22aa72c49ceff3e7ea7747cf7017cd86
irisklok <iris.studie@tetka.net>
2026-09-04 13:27:38 +0200

    Mary to p5 and 6

--- challenge_day1/bonus_story/Paragraph5.py
+++ challenge_day1/bonus_story/Paragraph5.py
@@ -1,19 +1,19 @@
 #ACT 1
 def act1_par5():
-    print ("“I can’t help it c4, I was born this way,” Jesus responds and continues crying. " \
+    print ("“I can’t help it Mary, I was born this way,” Jesus responds and continues crying. " \
     "Lady Gaga gives a proud look, while Steve, Judas, and the c2 start clapping.")
 
 
 #ACT 2
 def act2_par5():
     print ("Jesus and Judas can still faintly hear c3’s song of Judas, but they try to ignore her. " \
-    "c4 and the c2 watch and Judas kisses Jesus’ cheek and murmurs something, " \
+    "Mary and the c2 watch and Judas kisses Jesus’ cheek and murmurs something, " \
     "but are then horrified when Jesus is arrested. Steve perks up at the commotion and stops eating.")
 
 
 #ACT 3
 def act3_par5(): 
-    print ("There’s a happy shout as c4 and Judas give Jesus a hug. " \
+    print ("There’s a happy shout as Mary and Judas give Jesus a hug. " \
     "Lady Gaga bursts into song, accompanying Steve’s guitar. " \
     "“I can’t remember, but it’s alright a-alright. Just Dance!” " \
     "The c2 summons a dance floor and lights.")
