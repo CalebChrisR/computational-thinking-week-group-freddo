f35753b9276c20900744349f2d25d985649b0f48
amalia1131 <amalia.gerg@gmail.com>
2026-09-04 12:37:06 +0200

    changed all c5s to Judas

--- challenge_day1/bonus_story/Paragraph3.py
+++ challenge_day1/bonus_story/Paragraph3.py
@@ -3,20 +3,20 @@
 def act1_par3 ():
     print("As c4 predicted, c1 starts screaming, because he is a newborn baby. " \
 "The c2 gives him strength to scream loud. Lady Gaga cannot outsing c1 right now, " \
-"and c5 is perplexed by how loud c1 is. c6 pulls down the witch hat over his ears to block out the noise.") 
+"and Judas is perplexed by how loud c1 is. c6 pulls down the witch hat over his ears to block out the noise.") 
 
 # Act 2
 def act2_par3 ():
     print("c6 nods, “I am sure it is…” He indicates to the food, “I want your everything as long as it is free.” c1 gestures" \
     " at the food and nods. Realising that another person is joining their dinner, the c2 sets a new place for c6." \
-    " c5 starts getting nervous as he realises that time is running out for his betrayal. c4 and Lady Gaga are still gossiping about the local dog race.")
+    " Judas starts getting nervous as he realises that time is running out for his betrayal. c4 and Lady Gaga are still gossiping about the local dog race.")
 
 
 
 # Act 3
 def act3_par3 ():
     print("The c2 calls on all the good memories they shared with c1 and the gang." \
-    " They think of c4's devotion, Lady Gaga’s singing prowess, c5 and c1' relationship, and " \
+    " They think of c4's devotion, Lady Gaga’s singing prowess, Judas and c1' relationship, and " \
     "finally c6's eccentricness. With a pop, the wicked witch hat suddenly looks much better. It can probably travel through time now. ")
 
 act1_par3()
--- challenge_day1/bonus_story/Paragraph4.py
+++ challenge_day1/bonus_story/Paragraph4.py
@@ -1,25 +1,25 @@
 #ACT 1
 def act1_par4():
     print ("Mary asks ch1, “why cant you stop screaming.” " \
-    "ch3 stops singing ch5 so that they can hear Jesus answer. " \
+    "ch3 stops singing Judas so that they can hear Jesus answer. " \
     "ch2 passes through ch1 to quiet ch1’ screaming so that he can answer. " \
     "ch6 fiddles with his witch hat.")
 
 #ACT 2
 def act2_par4():
-    print ("“Hey, can we talk?” ch5 says and pulls ch1 aside. " \
-    "They walk to the garden to escape ch3's singing of the song ch5. " \
+    print ("“Hey, can we talk?” Judas says and pulls ch1 aside. " \
+    "They walk to the garden to escape ch3's singing of the song Judas. " \
     "Everyone notices them leaving, and peek out the window. " \
     "ch6, however, knowing what happens, stays sat at the table, enjoying the meal. " \
     "Mary and ch2 give each other the side eye, " \
-    "when they see ch1 and ch5 standing near each other. ")
+    "when they see ch1 and Judas standing near each other. ")
 
 #ACT 3
 def act3_par4():
     print ("ch6 takes the hat, and with a pop suAmmons a guitar. " \
     "He starts strumming it, much to the enjoyment of ch2. " \
     "ch3 loves the song, and starts thinking of a way to accompany him. " \
-    "ch5 and Mary turn back to the resting place in awe as the stone at ch1’ cave moves until suddenly, ch1 it out. " \
+    "Judas and Mary turn back to the resting place in awe as the stone at ch1’ cave moves until suddenly, ch1 it out. " \
     "“Did you miss me?”")
 
 act1_par4()
