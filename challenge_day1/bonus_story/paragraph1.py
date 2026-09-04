a2c625db4b7086da5e8e7e531d4a243c9c8af480
unit-fam <helenkttw@gmail.com>
2026-09-04 12:28:20 +0200

    added steve to par 1

--- challenge_day1/bonus_story/paragraph1.py
+++ challenge_day1/bonus_story/paragraph1.py
@@ -2,8 +2,8 @@
 
 def act1_par1():
     print(
-        "ch1 is born in the manger. his mother, ch4, just knows that the ch2 is with him." \
-        "ch3 is dancing to asong which calls on ch5. ch6 is vibing the corner, doing an interpretive dance. It seems wicked..."
+        "ch1 is born in the manger. His mother, ch4, just knows that the ch2 is with him." \
+        "ch3 is dancing to a song which calls on ch5. Steve is vibing the corner, doing an interpretive dance. It seems wicked..."
     )
     return
 def act2_par1():
@@ -12,14 +12,14 @@ def act2_par1():
         "The ch2 allows water to turn to wine, and they are all fed. " \
         "ch4 and ch3 sit in the corner sipping on some spirit (not holy), " \
         "ch4 adds tomato juice to hers. 'This is kind of good,'ch3 remarks after trying it." \
-        "'It's bloody good even!' said ch6 as he burst into existence, 'can I join this last supper?"
+        "'It's bloody good even!' says Steve as he bursts into existence, 'can I join this last supper?"
     )
 
     return
 
 def act3_par1():
     print(
-        "ch6 appears with a burst of light. His witch hat looks a little sad, he can't travel through time anymore :(." \
+        "Steve appears with a burst of light. His witch hat looks a little sad, he can't travel through time anymore :(." \
         "ch4, ch5, and the ch2 wait outside ch1' resting place." \
         "ch3, for once, is not singing, understanding the serious situation.)"
     )
