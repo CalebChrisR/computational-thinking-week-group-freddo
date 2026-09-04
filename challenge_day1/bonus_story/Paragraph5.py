efd3a5f01d8f227bf4a282775d174057fb8415c0
unit-fam <helenkttw@gmail.com>
2026-09-04 15:15:17 +0200

    maybe work

--- challenge_day1/bonus_story/Paragraph5.py
+++ challenge_day1/bonus_story/Paragraph5.py
@@ -1,22 +1,22 @@
 #ACT 1
 def act1_par5():
-    print ("'I can’t help it Mary, I was born this way,' Jesus responds and continues crying. " \
-    "Lady Gaga gives a proud look, while Steve, Judas, and the Holy Spirit start clapping.")
+    return ("'I can’t help it ch4, I was born this way,' ch1 responds and continues crying. " \
+    "ch3 gives a proud look, while ch6, Judas, and the ch2 start clapping.")
 
 
 #ACT 2
 def act2_par5():
-    print ("Jesus and Judas can still faintly hear Lady Gaga’s song of Judas, but they try to ignore her. " \
-    "Mary and the Holy Spirit watch and Judas kisses Jesus’ cheek and murmurs something, " \
-    "but are then horrified when Jesus is arrested. Steve perks up at the commotion and stops eating.")
+    return ("ch1 and Judas can still faintly hear ch3’s song of Judas, but they try to ignore her. " \
+    "ch4 and the ch2 watch and Judas kisses ch1’ cheek and murmurs something, " \
+    "but are then horrified when ch1 is arrested. ch6 perks up at the commotion and stops eating.")
 
 
 #ACT 3
 def act3_par5(): 
-    print ("There’s a happy shout as Mary and Judas give Jesus a hug. " \
-    "Lady Gaga bursts into song, accompanying Steve’s guitar. " \
+    return ("There’s a happy shout as ch4 and Judas give Jesus a hug. " \
+    "ch3 bursts into song, accompanying ch6's guitar. " \
     "'I can’t remember, but it’s alright a-alright. Just Dance!' " \
-    "The Holy Spirit summons a dance floor and lights.")
+    "The ch2 summons a dance floor and lights.")
 
 if __name__ == "__main__":
     act1_par5()
