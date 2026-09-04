d68cd5beceee3e4754710fb538e0acb23e78e3dc
unit-fam <helenkttw@gmail.com>
2026-09-04 12:52:00 +0200

    Added steve to ALL pars

--- challenge_day1/bonus_story/Paragraph4.py
+++ challenge_day1/bonus_story/Paragraph4.py
@@ -3,20 +3,20 @@ def act1_par4():
     print ("Mary asks Jesus, “why cant you stop screaming.” " \
     "ch3 stops singing Judas so that they can hear Jesus answer. " \
     "ch2 passes through Jesus to quiet Jesus’ screaming so that he can answer. " \
-    "ch6 fiddles with his witch hat.")
+    "Steve fiddles with his witch hat.")
 
 #ACT 2
 def act2_par4():
     print ("“Hey, can we talk?” Judas says and pulls Jesus aside. " \
     "They walk to the garden to escape ch3's singing of the song Judas. " \
     "Everyone notices them leaving, and peek out the window. " \
-    "ch6, however, knowing what happens, stays sat at the table, enjoying the meal. " \
+    "Steve, however, knowing what happens, stays sat at the table, enjoying the meal. " \
     "Mary and ch2 give each other the side eye, " \
     "when they see Jesus and Judas standing near each other. ")
 
 #ACT 3
 def act3_par4():
-    print ("ch6 takes the hat, and with a pop suAmmons a guitar. " \
+    print ("Steve takes the hat, and with a pop suAmmons a guitar. " \
     "He starts strumming it, much to the enjoyment of ch2. " \
     "ch3 loves the song, and starts thinking of a way to accompany him. " \
     "Judas and Mary turn back to the resting place in awe as the stone at Jesus’ cave moves until suddenly, Jesus it out. " \
--- challenge_day1/bonus_story/Paragraph5.py
+++ challenge_day1/bonus_story/Paragraph5.py
@@ -1,20 +1,20 @@
 #ACT 1
 def act1_par5():
     print ("“I can’t help it c4, I was born this way,” Jesus responds and continues crying. " \
-    "c3 gives a proud look, while c6, Judas, and the c2 start clapping.")
+    "c3 gives a proud look, while Steve, Judas, and the c2 start clapping.")
 
 
 #ACT 2
 def act2_par5():
     print ("Jesus and Judas can still faintly hear c3’s song of Judas, but they try to ignore her. " \
     "c4 and the c2 watch and Judas kisses Jesus’ cheek and murmurs something, " \
-    "but are then horrified when Jesus is arrested. c6 perks up at the commotion and stops eating.")
+    "but are then horrified when Jesus is arrested. Steve perks up at the commotion and stops eating.")
 
 
 #ACT 3
 def act3_par5(): 
     print ("There’s a happy shout as c4 and Judas give Jesus a hug. " \
-    "c3 bursts into song, accompanying c6’s guitar. " \
+    "c3 bursts into song, accompanying Steve’s guitar. " \
     "“I can’t remember, but it’s alright a-alright. Just Dance!” " \
     "The c2 summons a dance floor and lights.")
 
