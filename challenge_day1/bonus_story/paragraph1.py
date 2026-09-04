8f393c763741522492666405b33d4120562822c1
CalebChrisR <christopher.rojer@gmail.com>
2026-09-04 13:48:00 +0200

    holy spirit in par 1

--- challenge_day1/bonus_story/paragraph1.py
+++ challenge_day1/bonus_story/paragraph1.py
@@ -3,14 +3,14 @@
 
 def act1_par1():
     print(
-        "Jesus is born in the manger. His mother, Mary, just knows that the ch2 is with him." \
+        "Jesus is born in the manger. His mother, Mary, just knows that the Holy Spirit is with him." \
         "Lady Gaga is dancing to a song which calls on Judas. Steve is vibing in the corner, doing an interpretive dance. It seems wicked..."
     )
     return
 def act2_par1():
     print(
         "Time has passed. Jesus is hosting supper. Judas, however, knows it's the last supper for them all. " \
-        "The ch2 allows water to turn to wine, and they are all fed. " \
+        "The Holy Spirit allows water to turn to wine, and they are all fed. " \
         "Mary and Lady Gaga sit in the corner sipping on some spirit (not holy), " \
         "Mary adds tomato juice to hers. 'This is kind of good,'Lady Gaga remarks after trying it." \
         "'It's bloody good even!' says Steve as he bursts into existence, 'can I join this last supper?"
@@ -21,7 +21,7 @@ def act2_par1():
 def act3_par1():
     print(
         "Steve appears with a burst of light. His witch hat looks a little sad, he can't travel through time anymore :(." \
-        "Mary, Judas, and the ch2 wait outside Jesus' resting place." \
+        "Mary, Judas, and the Holy Spirit wait outside Jesus' resting place." \
         "Lady Gaga, for once, is not singing, understanding the serious situation."
     )
     return
